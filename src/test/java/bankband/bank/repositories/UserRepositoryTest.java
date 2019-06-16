7dc5bd1964dc055399653ee4567ac416ec102f02
Alena Dufková <dufa00@vse.cz>
2019-06-16 12:31:19 +0200

    Pridan UserRepositoryTest.itFindsAll

--- src/test/java/bankband/bank/repositories/UserRepositoryTest.java
+++ src/test/java/bankband/bank/repositories/UserRepositoryTest.java
@@ -5,6 +5,8 @@ import bankband.bank.models.User;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class UserRepositoryTest extends BaseTest {
@@ -73,5 +75,37 @@ public class UserRepositoryTest extends BaseTest {
         Integer id2 = repo.create(user2);
 
         assertEquals(user1, repo.findByEmail(user1.getEmail()));
+    }
+
+    @Test
+    public void itFindsAll() {
+        User user1 = new User();
+        user1.setName("Franta");
+        user1.setSurname("Sídlo");
+        user1.setEmail("franta@sadlo.cz");
+        user1.setPassword("password");
+
+        User user2 = new User();
+        user2.setName("Ana");
+        user2.setSurname("Frankova");
+        user2.setEmail("ffr@email.cz");
+        user2.setPassword("pass");
+
+        ArrayList<User> list = new ArrayList<>();
+
+        assertEquals(list, repo.findAll());
+
+
+        Integer id1 = repo.create(user1);
+
+        list.add(user1);
+        assertEquals(list, repo.findAll());
+
+
+        Integer id2 = repo.create(user2);
+
+
+        list.add(user2);
+        assertEquals(list, repo.findAll());
     }
 }
