a572112bb3d60480d6af70536be6714995214872
Alena Dufková <dufa00@vse.cz>
2019-06-16 11:37:47 +0200

    Test, který kontroluje funkci findById v UserRepository

--- src/test/java/bankband/bank/repositories/UserRepositoryTest.java
+++ src/test/java/bankband/bank/repositories/UserRepositoryTest.java
@@ -5,7 +5,7 @@ import bankband.bank.models.User;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class UserRepositoryTest extends BaseTest {
 
@@ -27,5 +27,29 @@ public class UserRepositoryTest extends BaseTest {
         Integer id = repo.create(user);
 
         assertNotNull(id);
+    }
+
+    @Test
+    public void itFindsById() {
+        User user1 = new User();
+        user1.setName("Franta");
+        user1.setSurname("Sídlo");
+        user1.setEmail("franta@sadlo.cz");
+        user1.setPassword("password");
+
+        User user2 = new User();
+        user2.setName("Ana");
+        user2.setSurname("Frankova");
+        user2.setEmail("ffr@email.cz");
+        user2.setPassword("pass");
+
+        Integer id1 = repo.create(user1);
+        Integer id2 = repo.create(user2);
+
+        assertNotEquals(id1, id2);
+
+        assertEquals(user1, repo.findById(id1));
+        assertEquals(user2, repo.findById(id2));
+        assertEquals(null, repo.findById(123));
     }
 }
